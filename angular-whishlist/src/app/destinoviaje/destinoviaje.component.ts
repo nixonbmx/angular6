6e8ab7965cee123d6646b42a8aaafd7a58799a50
nixon piñeres <napineres@misena.edu.co>
2021-06-30 13:53:51 -0500

    prototipar y personalizar vistas

--- angular-whishlist/src/app/destinoviaje/destinoviaje.component.ts
+++ angular-whishlist/src/app/destinoviaje/destinoviaje.component.ts
@@ -8,6 +8,7 @@ import { destinoviaje } from '../models/destino-viaje.model';
 })
 export class DestinoviajeComponent implements OnInit {
  @Input() destino: destinoviaje;
+ @Input('idx') position: number;
  @HostBinding ('attr.class') cssClass = 'col-md-4';
  @Output() clicked: EventEmitter<destinoviaje>;
   constructor() {
